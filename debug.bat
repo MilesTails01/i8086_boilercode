32e6d5884aa8abd4434119f838e610c2e67b7455
Tommy <83541965+MilesTails01@users.noreply.github.com>
2024-07-18 11:32:31 +0900

    :sparkles: Create debug.bat

---
+++ debug.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set PROJECT_ROOT=%cd%
+set DOSBOX_PATH=%PROJECT_ROOT%\bin\DOSBOX-0.74-3\DOSBox.exe
+set CONFIG_FILE=%PROJECT_ROOT%\bin\DOSBOX-0.74-3\dosbox.conf
+cd /d %PROJECT_ROOT%
+
+%DOSBOX_PATH%	-conf %CONFIG_FILE% ^
+				-c "mount c %PROJECT_ROOT%" ^
+				-c "c:" ^
+				-c "bin\AFDEBUG.EXE dist\MAIN.EXE" 
